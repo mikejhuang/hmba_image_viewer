2a96290cab6efb8e825795ae2f6f18db3bfe1ab2
Jasmine Herri <jasmineherri@gmail.com>
2023-07-07 09:28:55 -0700

    Added __eq__ and __lt__ and sort by tree level

--- sandbox/jasmine_work/csv_builder/Specimen.py
+++ sandbox/jasmine_work/csv_builder/Specimen.py
@@ -22,3 +22,34 @@ class Specimen:
 
     def __str__(self):
         return "specimen_id: " + self.specimen_id + "\t" + "parent_id: " + self.parent_specimen_id
+    
+    def __eq__(self, other):
+        if isinstance(other, Specimen):
+            self_suffix = self.__get_suffix()
+            other_suffix = other.__get_suffix()
+
+            return (len(self.parent_specimen_name) == len(other.parent_specimen_name)) and (self_suffix == other_suffix)   
+        
+        return False
+
+    def __lt__(self, other):
+        if isinstance(other, Specimen):
+            if len(self.parent_specimen_name) < len(other.parent_specimen_name):
+                return True
+            elif len(self.parent_specimen_name) == len(other.parent_specimen_name):
+                #get the numerical suffix of each parent_specimen_name for comparison
+                self_suffix = self.__get_suffix()
+                other_suffix = other.__get_suffix() 
+                
+                if (self_suffix != "CX" != 0) and (other_suffix != "CX" != 0):
+                    return int(self_suffix) < int(other_suffix)
+                elif self_suffix != "CX" and other_suffix == "CX":
+                    return True
+                        
+        return False
+
+    # get numerical suffix of parent_specimen_name
+    def __get_suffix(self):
+        if len(self.parent_specimen_name) > 0:
+            return self.parent_specimen_name[len(self.parent_specimen_name) - 2:]
+        return 0
