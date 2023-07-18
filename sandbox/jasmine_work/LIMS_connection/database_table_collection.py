75054f6780bec5a377e84f8466beabf47cdb0e29
Jasmine Herri <jasmineherri@gmail.com>
2023-07-18 13:59:48 -0700

    Working backend and can get list of all tables/col

---
+++ sandbox/jasmine_work/LIMS_connection/database_table_collection.py
@@ -0,0 +1,25 @@
+import sqlalchemy 
+import psycopg2
+import pandas as pd
+from Specimen import Specimen
+from csv import DictReader
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy import inspect
+
+def main():
+
+    engine = sqlalchemy.create_engine(
+    "postgresql+psycopg2://LIMSUSER:LIMSPWD@LIMSALIAS/lims2")
+
+    Base = automap_base()
+    Base.prepare(engine, reflect=True)
+
+    inspector = inspect(engine)
+    hello = 1
+    # for table_name in Base.classes.keys():
+    #     print(table_name)
+    for column in inspector.get_columns("specimens"):
+        print(str(hello) + ". " + column['name'] + " type: " + str(type(column['name'])))
+        hello = hello + 1
+
+main()
