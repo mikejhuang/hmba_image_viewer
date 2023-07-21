79f9e89381de8f17eacecebbe766e07cbb19a1d8
Jasmine Herri <jasmineherri@gmail.com>
2023-07-21 15:42:12 -0700

    Creates one level of dropdown hierarchy

--- sandbox/jasmine_work/flask_app/app.py
+++ sandbox/jasmine_work/flask_app/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, jsonify, request, json
 from flask_bootstrap import Bootstrap5
 from flask_wtf import FlaskForm, CSRFProtect
 from wtforms import StringField, SubmitField
@@ -115,22 +115,6 @@ class Donor(db.Model):
     occupation_id = db.Column(db.String)
     water_restricted = db.Column(db.String)
 
-# class Parent(db.Model):
-#     __tablename__ = 'parents'
-#     id = db.Column(db.String)
-#     name = db.Column(db.String, primary_key = True)
-#     plane_of_section_id = db.Column(db.String)
-#     hemisphere_id = db.Column(db.String)
-#     parent_x_coord = db.Column(db.String)
-#     parent_y_coord = db.Column(db.String)
-#     parent_z_coord = db.Column(db.String)
-#     project_id = db.Column(db.String)
-#     donor_id = db.Column(db.String) 
-#     parent_id = db.Column(db.String)
-
-#     # not already given in Specimen class
-#     children = db.relationship('Specimen', backref = 'parents', lazy=True)
-
 class DonorForm(FlaskForm):
     donor = StringField('What is the donor ID of the specimens you would like to see?', validators=[DataRequired(), Length(10, 40)])
     submit = SubmitField('Submit')
@@ -162,19 +146,6 @@ def home():
 @app.route('/specimens/<donor_id>/')
 def specimens(donor_id):
 
-    # parents = db.Table('parents',
-    #     db.Column('specimen_id', db.String, db.ForeignKey('specimens.id'), primary_key=True),
-    #     db.Column('specimen_name', db.String, db.ForeignKey('specimens.name'), primary_key=True),
-    #     db.Column('specimen_plane_id', db.String, db.ForeignKey('specimens.plane_of_section_id')),
-    #     db.Column('specimen_hemisphere_id', db.String, db.ForeignKey('specimens.hemisphere_id')),
-    #     db.Column('specimen_parent_x_coord', db.String, db.ForeignKey('specimens.parent_x_coord')),
-    #     db.Column('specimen_parent_y_coord', db.String, db.ForeignKey('specimens.parent_y_coord')),
-    #     db.Column('specimen_parent_z_coord', db.String, db.ForeignKey('specimens.parent_z_coord')),
-    #     db.Column('specimen_project_id', db.String, db.ForeignKey('specimens.project_id')),
-    #     db.Column('specimen_donor_id', db.String, db.ForeignKey('specimens.donor_id')),
-    #     db.Column('specimen_parent_id', db.String, db.ForeignKey('specimens.parent_id')),
-    #     db.Column('specimen_children', db.String, db.ForeignKey('specimens.')))
-
     # get table of specimens matching given donor id (has all columns!)
     specimens = db.session.execute(db.select(Specimen)
             .filter_by(donor_id=donor_id)
@@ -187,9 +158,10 @@ def specimens(donor_id):
             .order_by(Specimen.name)).scalars()
     
     combined_data = combine_data(specimens, relationships)
-    
+    # tree = build_tree(combined_data, None)
+
     # renders template that displays all specimens in table with their id and parent id
-    return render_template('list.html', donor_id=donor_id, specimens=combined_data)
+    return render_template('dropdown.html', data=combined_data)
 
 # specimens = table of all specimens with given donor_id
 # maps specimen ids to children
@@ -232,6 +204,16 @@ def combine_data(specimens, relationships):
 
     return combined_data
 
+# def build_tree(combined_data, parent=None):
+#     children =[]
+
+#     for specimen in combined_data:
+#         if specimen.get('parent_id') == parent.id:
+#             children = build_tree(combined_data, specimen["id"])
+#             if children:
+#                 specimen["children"] = children
+#             tree.append(specimen)
+#     return tree
 
 if __name__ == '__main__':
     db.create_all()
