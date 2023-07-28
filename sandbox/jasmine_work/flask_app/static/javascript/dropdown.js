8806bb8a093783c3918ddc2828073a979933e1af
Jasmine Herri <jasmineherri@gmail.com>
2023-07-28 12:07:46 -0700

    Dropdown fully working!

--- sandbox/jasmine_work/flask_app/static/javascript/dropdown.js
+++ sandbox/jasmine_work/flask_app/static/javascript/dropdown.js
@@ -3,10 +3,7 @@ var i;
 
 for (i = 0; i < toggler.length; i++) {
   toggler[i].addEventListener("click", function() {
-    var nestedElem = this.parentElement.querySelector(".nested");
-    if (!nestedElem.classList.contains("active")) {
-      nestedElem.classList.add("active");
-    }
-    this.classList.add("caret-down"); // always rotate caret on click
+    this.parentElement.querySelector(".nested").classList.toggle("active");
+    this.classList.toggle("caret-down");
   });
 }
