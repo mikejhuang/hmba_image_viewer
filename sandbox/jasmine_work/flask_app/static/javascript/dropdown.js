0501ee7664c10b080d6b2ddc983502b0c7c8fe55
Jasmine Herri <jasmineherri@gmail.com>
2023-08-01 12:56:06 -0700

    image rendering fully working! App at full basic functionality!!

--- sandbox/jasmine_work/flask_app/static/javascript/dropdown.js
+++ sandbox/jasmine_work/flask_app/static/javascript/dropdown.js
@@ -39,22 +39,20 @@ function updateSpecimenData(data) {
         });
     }
 
+    let placeholder = document.querySelector('#image-placeholder');
+    let imgElem = placeholder.querySelector('img'); // get image within placeholder
+
     // If the image_url is not 'None', add or update the image
     if (data.image_url !== 'None') {
-        let imgElem = document.querySelector('img[data-key="image_url"]');
-        let placeholder = document.querySelector('#image-placeholder');
         if (!imgElem) {
             // Create a new image element if it doesn't exist
             imgElem = document.createElement('img');
-            imgElem.dataset.key = "image_url";
             imgElem.alt = "Specimen image";
             imgElem.height = "550";
+            placeholder.appendChild(imgElem); // append it to placeholder
         }
         // Update the image source
         imgElem.src = data.image_url;
-
-        // Append the image to the placeholder div
-        placeholder.appendChild(imgElem);
     } else {
         // If there is no image for the specimen, remove the existing image if it exists
         if (imgElem) {
