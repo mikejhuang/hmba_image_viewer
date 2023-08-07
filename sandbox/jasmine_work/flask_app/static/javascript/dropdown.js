a161ddbda84bf0efcf43c501dd8cf4eecc946800
Jasmine Herri <jasmineherri@gmail.com>
2023-08-07 12:11:12 -0700

    Added button to expand and collapse entire dropdown menu

--- sandbox/jasmine_work/flask_app/static/javascript/dropdown.js
+++ sandbox/jasmine_work/flask_app/static/javascript/dropdown.js
@@ -2,17 +2,17 @@ var caretItems = document.getElementsByClassName("caret");
 var i;
 
 for (i = 0; i < caretItems.length; i++) {
-  caretItems[i].addEventListener("click", function() {
-    this.parentElement.querySelector(".nested").classList.toggle("active");
-    this.classList.toggle("caret-down");
-  });
+    caretItems[i].addEventListener("click", function() {
+        this.parentElement.querySelector(".nested").classList.toggle("active");
+        this.classList.toggle("caret-down");
+    });
 }
 
 var clickableItems = document.getElementsByClassName("clickable");
 
 // Add a click event listener to each clickable element
 for (i = 0; i < clickableItems.length; i++) {
-  clickableItems[i].addEventListener("click", function() {
+    clickableItems[i].addEventListener("click", function() {
         var nodeName = this.textContent;
 
         fetch('/get_specimen_data', {
@@ -29,37 +29,31 @@ for (i = 0; i < clickableItems.length; i++) {
 
 // Function to update specimen data on the page
 function updateSpecimenData(data) {
-    // Add keys and values from data to the table
-    for (let key in data) {
-        var cells = document.querySelectorAll(`td[data-key="${key}"], strong[data-key="${key}"]`);
-        cells.forEach(cell => {
-            if (cell) {
-                cell.textContent = data[key];
-            }
-        });
-    }
-
-    let placeholder = document.querySelector('#image-placeholder');
-    let imgElem = placeholder.querySelector('img'); // get image within placeholder
-
-    // If the image_url is not 'None', add or update the image
-    if (data.image_url !== 'None') {
-        if (!imgElem) {
-            // Create a new image element if it doesn't exist
-            imgElem = document.createElement('img');
-            imgElem.alt = "Specimen image";
-            imgElem.height = "550";
-            placeholder.appendChild(imgElem); // append it to placeholder
-        }
-        // Update the image source
-        imgElem.src = data.image_url;
-    } else {
-        // If there is no image for the specimen, remove the existing image if it exists
-        if (imgElem) {
-            imgElem.remove();
-        }
-    }
-
+    // Code
 }
 
+var toggleButton = document.getElementById("toggleDropdown");
 
+toggleButton.addEventListener("click", function(){
+    var anyOpen = Array.from(caretItems).some(function(caret) {
+        return caret.classList.contains('caret-down');
+    });
+
+    if(anyOpen){
+        // If any dropdowns are open, close them all
+        for (i = 0; i < caretItems.length; i++) {
+            if (caretItems[i].classList.contains('caret-down')) {
+                caretItems[i].click();
+            }
+        }
+        toggleButton.textContent = 'Expand All';
+    } else {
+        // If all dropdowns are closed, open them all
+        for (i = 0; i < caretItems.length; i++) {
+            if (!caretItems[i].classList.contains('caret-down')) {
+                caretItems[i].click();
+            }
+        }
+        toggleButton.textContent = 'Collapse All';
+    }
+});
