a3d944d10aba7dc3a1551e665c46b23b74165b4d
Jasmine Herri <jasmineherri@gmail.com>
2023-08-08 10:49:57 -0700

    Displays multiple images but not in carousel, just as a gallery

--- sandbox/jasmine_work/flask_app/static/javascript/dropdown.js
+++ sandbox/jasmine_work/flask_app/static/javascript/dropdown.js
@@ -38,28 +38,29 @@ function updateSpecimenData(data) {
             }
         });
     }
+    // OG image code, will display one image even if there are multiple 
+    // let placeholder = document.querySelector('#image-placeholder');
+    // let imgElem = placeholder.querySelector('img'); // get image within placeholder
 
-    let placeholder = document.querySelector('#image-placeholder');
-    let imgElem = placeholder.querySelector('img'); // get image within placeholder
+    // // If the image_url is not 'None', add or update the image
+    // if (data.image_url !== 'None') {
+    //     if (!imgElem) {
+    //         // Create a new image element if it doesn't exist
+    //         imgElem = document.createElement('img');
+    //         imgElem.alt = "Specimen image";
+    //         imgElem.height = "550";
+    //         placeholder.appendChild(imgElem); // append it to placeholder
+    //     }
+    //     // Update the image source
+    //     imgElem.src = data.image_url[0];
+    // } else {
+    //     // If there is no image for the specimen, remove the existing image if it exists
+    //     if (imgElem) {
+    //         imgElem.remove();
+    //     }
+    // }
 
-    // If the image_url is not 'None', add or update the image
-    if (data.image_url !== 'None') {
-        if (!imgElem) {
-            // Create a new image element if it doesn't exist
-            imgElem = document.createElement('img');
-            imgElem.alt = "Specimen image";
-            imgElem.height = "550";
-            placeholder.appendChild(imgElem); // append it to placeholder
-        }
-        // Update the image source
-        imgElem.src = data.image_url[0];
-    } else {
-        // If there is no image for the specimen, remove the existing image if it exists
-        if (imgElem) {
-            imgElem.remove();
-        }
-    }
-
+    // attempt at displaying multiple images in carousel, doesn't work
     // window.addEventListener('DOMContentLoaded', (event) => {
     //     let placeholder = document.querySelector('#image-placeholder');
     //     let imgElem = placeholder.querySelector('img'); // get image within placeholder
@@ -111,6 +112,42 @@ function updateSpecimenData(data) {
     
     //     updateImage();  // Initial image update
     // });
+
+    // attempt at displaying multiple images if there are multiple all at once, no carousel
+    let placeholder = document.querySelector('#image-placeholder');
+
+    // If the image_url is not 'None', add or update the image
+    if (data.image_url !== 'None') {
+        // First, clear any previous images
+        while (placeholder.firstChild) {
+            placeholder.firstChild.remove();
+        }
+        // If data.image_url is an array
+        if (Array.isArray(data.image_url)) {
+            // Loop through each URL in the array
+            for (let url of data.image_url) {
+                // Create a new img element for each URL
+                let imgElem = document.createElement('img');
+                imgElem.alt = "Specimen image";
+                imgElem.height = "550";
+                imgElem.src = url; // Set the source of the image to the URL
+                placeholder.appendChild(imgElem); // Append the img element to the placeholder
+            }
+        } else {
+            // If data.image_url is a single string
+            let imgElem = document.createElement('img');
+            imgElem.alt = "Specimen image";
+            imgElem.height = "550";
+            imgElem.src = data.image_url; // Set the source of the image to the URL
+            placeholder.appendChild(imgElem); // Append the img element to the placeholder
+        }
+    } else {
+        // If there is no image for the specimen, remove the existing image if it exists
+        while (placeholder.firstChild) {
+            placeholder.firstChild.remove();
+        }
+    }
+
     
 }
 
