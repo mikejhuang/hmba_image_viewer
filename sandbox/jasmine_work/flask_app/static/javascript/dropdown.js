9daedfeb9b45fa54137a9c360ed8d58487c4dc6c
Jasmine Herri <jasmineherri@gmail.com>
2023-08-08 13:48:04 -0700

    Semi-working carousel, shows all images at once but specimens with only one image are good (a bit off-center but good)

--- sandbox/jasmine_work/flask_app/static/javascript/dropdown.js
+++ sandbox/jasmine_work/flask_app/static/javascript/dropdown.js
@@ -60,61 +60,48 @@ function updateSpecimenData(data) {
     //     }
     // }
 
-    // attempt at displaying multiple images in carousel, doesn't work
-    // window.addEventListener('DOMContentLoaded', (event) => {
-    //     let placeholder = document.querySelector('#image-placeholder');
-    //     let imgElem = placeholder.querySelector('img'); // get image within placeholder
-    
-    //     if (!imgElem) {
-    //         // Create a new image element if it doesn't exist
-    //         imgElem = document.createElement('img');
+    // attempt at displaying multiple images if there are multiple all at once, no carousel
+    // let placeholder = document.querySelector('#image-placeholder');
+
+    // // If the image_url is not 'None', add or update the image
+    // if (data.image_url !== 'None') {
+    //     // First, clear any previous images
+    //     while (placeholder.firstChild) {
+    //         placeholder.firstChild.remove();
+    //     }
+    //     // If data.image_url is an array
+    //     if (Array.isArray(data.image_url)) {
+    //         // Loop through each URL in the array
+    //         for (let url of data.image_url) {
+    //             // Create a new img element for each URL
+    //             let imgElem = document.createElement('img');
+    //             imgElem.alt = "Specimen image";
+    //             imgElem.height = "550";
+    //             imgElem.src = url; // Set the source of the image to the URL
+    //             placeholder.appendChild(imgElem); // Append the img element to the placeholder
+    //         }
+    //     } else {
+    //         // If data.image_url is a single string
+    //         let imgElem = document.createElement('img');
     //         imgElem.alt = "Specimen image";
     //         imgElem.height = "550";
-    //         placeholder.appendChild(imgElem); // append it to placeholder
+    //         imgElem.src = data.image_url; // Set the source of the image to the URL
+    //         placeholder.appendChild(imgElem); // Append the img element to the placeholder
     //     }
-    
-    //     // Event listeners for navigation buttons
-    //     let prevButton = document.querySelector('#prev');
-    //     let nextButton = document.querySelector('#next');
-    
-    //     // Get the first element with the class "image-container"
-    //     let container = document.querySelector('.image-container');
-    
-    //     // Get the images from the data-images attribute
-    //     // Parse it to get a list of images
-    //     let images = JSON.parse(container.getAttribute('data-images'));
-    //     let counter = 0;
-    
-    //     function updateImage() {
-    //         // If there is no image for the specimen, remove the existing image if it exists
-    //         if (images[counter] === 'None') {
-    //             imgElem.remove();
-    //         } else {
-    //             imgElem.src = images[counter];
-    //         }
+    // } else {
+    //     // If there is no image for the specimen, remove the existing image if it exists
+    //     while (placeholder.firstChild) {
+    //         placeholder.firstChild.remove();
     //     }
+    // }
     
-    //     prevButton.addEventListener('click', function() {
-    //         counter--;
-    //         if (counter < 0) {
-    //             counter = images.length - 1;
-    //         }
-    //         updateImage();
-    //     });
-    
-    //     nextButton.addEventListener('click', function() {
-    //         counter++;
-    //         if (counter >= images.length) {
-    //             counter = 0;
-    //         }
-    //         updateImage();
-    //     });
-    
-    //     updateImage();  // Initial image update
-    // });
-
-    // attempt at displaying multiple images if there are multiple all at once, no carousel
     let placeholder = document.querySelector('#image-placeholder');
+    let carousel = document.querySelector('#carousel');
+    let prevButton = document.querySelector('#prev');
+    let nextButton = document.querySelector('#next');
+    let carouselContainer = document.querySelector('#carousel-container');
+
+    const SCROLL_AMOUNT = 550;
 
     // If the image_url is not 'None', add or update the image
     if (data.image_url !== 'None') {
@@ -122,32 +109,45 @@ function updateSpecimenData(data) {
         while (placeholder.firstChild) {
             placeholder.firstChild.remove();
         }
-        // If data.image_url is an array
-        if (Array.isArray(data.image_url)) {
-            // Loop through each URL in the array
-            for (let url of data.image_url) {
-                // Create a new img element for each URL
-                let imgElem = document.createElement('img');
-                imgElem.alt = "Specimen image";
-                imgElem.height = "550";
-                imgElem.src = url; // Set the source of the image to the URL
-                placeholder.appendChild(imgElem); // Append the img element to the placeholder
-            }
-        } else {
-            // If data.image_url is a single string
+
+        // Loop through each URL in the array
+        for (let url of data.image_url) {
             let imgElem = document.createElement('img');
             imgElem.alt = "Specimen image";
             imgElem.height = "550";
-            imgElem.src = data.image_url; // Set the source of the image to the URL
-            placeholder.appendChild(imgElem); // Append the img element to the placeholder
+            imgElem.src = url;
+            placeholder.appendChild(imgElem);
+        }
+
+        // Only show buttons if there are multiple images
+        if (data.image_url.length > 1) {
+            prevButton.classList.remove('hide');
+            nextButton.classList.remove('hide');
+            carouselContainer.style.gap = "10px";
+
+            prevButton.addEventListener('click', function() {
+                carousel.scrollLeft -= SCROLL_AMOUNT;
+            });
+
+            nextButton.addEventListener('click', function() {
+                carousel.scrollLeft += SCROLL_AMOUNT;
+            });
+        } else {
+            prevButton.classList.add('hide');
+            nextButton.classList.add('hide');
+            carouselContainer.style.gap = "0";
         }
     } else {
+        prevButton.classList.add('hide');
+        nextButton.classList.add('hide');
+
         // If there is no image for the specimen, remove the existing image if it exists
         while (placeholder.firstChild) {
             placeholder.firstChild.remove();
         }
     }
-
+    
+    
     
 }
 
