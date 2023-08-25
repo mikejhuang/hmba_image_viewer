7cdea9d44b28c39f3ebf431c3316595d7e57cce5
Jasmine Herri <jasmineherri@gmail.com>
2023-08-25 14:31:16 -0700

    Added comments to dropdown js

--- flask_app/static/javascript/dropdown.js
+++ flask_app/static/javascript/dropdown.js
@@ -39,194 +39,19 @@ function updateSpecimenData(data) {
         });
     }
 
-    // deployed app that i showed at poster presentation
-    // let placeholder = document.querySelector('#image-placeholder');
-    // const FALLBACK_IMAGE_URL = "/static/Images/ErrorImage.JPG";
-    
-    // function handleImageError(imgElem) {
-    //     imgElem.src = FALLBACK_IMAGE_URL;
-    // }
-    
-    // function initCarousel() {
-    //     $(placeholder).slick({
-    //         dots: true,
-    //         infinite: true,
-    //         speed: 500,
-    //         slidesToShow: 1,
-    //         adaptiveHeight: true
-    //     });
-    
-    //     $(placeholder).on('click', '.slick-slide', function(e) {
-    //         e.preventDefault(); 
-    //         const currentSlideIndex = $(this).data('slick-index');
-    //         openLightboxAt(currentSlideIndex);
-    //     });
-    // }
-    
-    // function destroyCarousel() {
-    //     if ($(placeholder).hasClass('slick-initialized')) {
-    //         $(placeholder).slick('unslick');
-    //     }
-    // }
-    
-    // function openLightboxAt(index) {
-    //     $.magnificPopup.open({
-    //         items: data.image_urls.map(image => {
-    //             let treatment = data.treatment[image] || "";  
-    //             let title = '<a href="#" class="lightbox-download-btn">Download</a>';
-    //             if (treatment && treatment !== "None") {
-    //                 title = treatment + ' ' + title;
-    //             }
-    //             return {
-    //                 src: image,
-    //                 title: title
-    //             };
-    //         }), 
-    //         type: 'image',
-    //         gallery: {
-    //             enabled: true
-    //         },
-    //         callbacks: {
-    //             open: function() {
-    //                 $('.mfp-figure').on('click', function(e) {
-    //                     if ($(e.target).closest('.mfp-close').length || $(e.target).closest('.lightbox-download-btn').length) {
-    //                         return;
-    //                     }
-    //                     e.stopPropagation();
-    //                     let imageUrl = $(this).find('img').attr('src');
-    //                     window.open(imageUrl, '_blank');
-    //                 });
-    
-    //                 // Delegate listener for download buttons
-    //                 $('body').on('click', '.lightbox-download-btn', function(e) {
-    //                     e.preventDefault();
-    //                     e.stopPropagation();
-    //                     let imageUrl = $('.mfp-img').attr('src');
-    //                     let activeSlideIndex = data.image_urls.indexOf(imageUrl); 
-    //                     let filename = data.all_image_names[activeSlideIndex].replace('.aff', '') + ".jpg";
-    //                     downloadImage(imageUrl, filename);
-    //                 });
-    //             },
-    //             close: function() {
-    //                 $('.mfp-figure').off('click');
-    //                 $('body').off('click', '.lightbox-download-btn'); // Unbind the download button delegate event
-    //             }
-    //         }
-    //     }, index);
-    // }
-    
-    // function downloadImage(url, filename) {
-    //     fetch(url)
-    //         .then(response => response.blob())
-    //         .then(blob => {
-    //             const link = document.createElement('a');
-    //             link.href = URL.createObjectURL(blob);
-    //             link.download = filename;
-    //             document.body.appendChild(link);
-    //             link.click();
-    //             document.body.removeChild(link);
-    //         });
-    // }
-    
-    // function downloadCurrentImage() {
-    //     let activeSlide = $(placeholder).slick('slickCurrentSlide');
-    //     let currentImageUrl = data.image_urls[activeSlide];
-    
-    //     let filename = data.all_image_names[activeSlide].replace('.aff', '') + ".jpg";
-    
-    //     downloadImage(currentImageUrl, filename);
-    // }
-    
-    // const createImageContainer = (url, hoverText) => {
-    //     let containerDiv = document.createElement('div');
-    //     containerDiv.className = "image-container";
-    
-    //     let anchorElem = document.createElement('a');
-    //     anchorElem.href = url;
-    //     anchorElem.className = "image-popup";
-    //     anchorElem.target = "_blank";
-    
-    //     anchorElem.addEventListener('click', function(e) {
-    //         e.preventDefault();
-    //         const index = data.image_urls.indexOf(url);
-    //         openLightboxAt(index);
-    //     });
-    
-    //     let imgElem = document.createElement('img');
-    //     imgElem.alt = "Specimen image";
-    //     imgElem.style.height = "550px"; 
-    //     imgElem.src = url;
-    //     imgElem.onerror = function() { handleImageError(imgElem); };
-    
-    //     anchorElem.appendChild(imgElem);
-    //     containerDiv.appendChild(anchorElem);
-    
-    //     if (hoverText && hoverText !== "None") {  
-    //         let overlayDiv = document.createElement('div');
-    //         overlayDiv.className = "image-overlay";
-    //         overlayDiv.innerHTML = hoverText;
-    
-    //         let overlayAnchor = document.createElement('a');
-    //         overlayAnchor.href = url;
-    //         overlayAnchor.className = 'image-popup';
-    //         overlayAnchor.style.display = 'block';
-    //         overlayAnchor.appendChild(overlayDiv);
-    
-    //         containerDiv.appendChild(overlayAnchor);
-    //     }
-    
-    //     return containerDiv;
-    // };
-    
-    // if (data.image_urls && data.image_urls !== 'None') {
-    //     destroyCarousel();
-    
-    //     while (placeholder.firstChild) {
-    //         placeholder.firstChild.remove();
-    //     }
-    
-    //     for (let imageUrl of data.image_urls) {
-    //         let hoverData = data.treatment[imageUrl] ? data.treatment[imageUrl] : "";
-    //         placeholder.appendChild(createImageContainer(imageUrl, hoverData));
-    //     }
-    
-    //     if (data.image_urls.length > 1) {
-    //         initCarousel();
-    
-    //         let downloadButton = document.createElement('a');
-    //         downloadButton.href = "#";
-    //         downloadButton.textContent = "Download";
-    //         downloadButton.className = "btn";
-    //         downloadButton.id = "download-button";
-    
-    //         downloadButton.addEventListener('click', function(e) {
-    //             e.preventDefault();
-    //             e.stopPropagation();
-    //             downloadCurrentImage();
-    //         });
-    
-    //         placeholder.appendChild(downloadButton);
-    
-    //     } else if (data.image_urls.length === 1) {
-    //         $(placeholder).find('.image-popup').on('click', function(e) {
-    //             e.preventDefault();
-    //             openLightboxAt(0);
-    //         });
-    //     }
-    
-    // } else {
-    //     destroyCarousel();
-    //     while (placeholder.firstChild) {
-    //         placeholder.firstChild.remove();
-    //     }
-    // }
+    // the error image that displays if the path in LIMS is invalid
     const FALLBACK_IMAGE_URL = "/static/Images/ErrorImage.JPG";
 
+    // alters the given image url with different zooms and then tests each one
+    // to see which one is valid
     function handleImageErrorWithAlterations(imgElem, originalUrl) {
         let alteredUrls = generateAlteredUrls(originalUrl);
         tryNextUrl(imgElem, alteredUrls, originalUrl);
     }
     
+    // alters the given image url with different zoom values 
+    // starts at higher zoom
+    // returns list of altered urls with zooms from 6-0 inclusive
     function generateAlteredUrls(originalUrl) {
         let alteredUrls = [];
         const charactersToTry = "6543210"; // Starts at 6 and ends at 0
@@ -239,6 +64,8 @@ function updateSpecimenData(data) {
         return alteredUrls;
     }
     
+    // tests the given image urls to see which ones are valid
+    // will update the image url with the highest zoom that is valid
     function tryNextUrl(imgElem, urls, originalUrl) {
         if (urls.length === 0) {
             // All URL alterations failed, use fallback
@@ -257,12 +84,13 @@ function updateSpecimenData(data) {
         }
     }
     
+    // retrives the image placeholder in the html file
+    // makes it so that we can render images for a specimen if they have them 
+    // in the right place on the webpage
     let placeholder = document.querySelector('#image-placeholder');
     
-    function handleImageError(imgElem) {
-        imgElem.src = FALLBACK_IMAGE_URL;
-    }
-    
+    // initializes the image carousel
+    // if the user clicks on the image the lightbox will open on that image
     function initCarousel() {
         $(placeholder).slick({
             dots: true,
@@ -286,6 +114,7 @@ function updateSpecimenData(data) {
         }
     }
     
+    // opens the image lightbox on the current image in the carousel
     function openLightboxAt(index) {
         // Use direct mapping of data.image_urls for the lightbox items
         $.magnificPopup.open({
@@ -314,7 +143,8 @@ function updateSpecimenData(data) {
                         let imageUrl = $(this).find('img').attr('src');
                         window.open(imageUrl, '_blank');
                     });
-    
+                    
+                    // downloads the current image
                     $('body').on('click', '.lightbox-download-btn', function(e) {
                         e.preventDefault();
                         e.stopPropagation();
@@ -332,7 +162,8 @@ function updateSpecimenData(data) {
         }, index);
     }
     
-    
+    // download the current imager with the original filename intact 
+    // will download the image as a jpg
     function downloadImage(url, filename) {
         fetch(url)
             .then(response => response.blob())
@@ -346,6 +177,8 @@ function updateSpecimenData(data) {
             });
     }
     
+    // download the current imager with the original filename intact 
+    // will download the image as a jpg
     function downloadCurrentImage() {
         let activeSlide = $(placeholder).slick('slickCurrentSlide');
         let currentImageUrl = data.image_urls[activeSlide];
@@ -355,6 +188,8 @@ function updateSpecimenData(data) {
         downloadImage(currentImageUrl, filename);
     }
     
+    // formats the images in the carousel to be the specified height of 550 px
+    // adds the hover over stain images so can see the type of stain used
     const createImageContainer = (url, hoverText) => {
         let containerDiv = document.createElement('div');
         containerDiv.className = "image-container";
@@ -397,18 +232,22 @@ function updateSpecimenData(data) {
         return containerDiv;
     };
     
+    // takes care of image rendering as the user goes through the specimen dropdown 
     if (data.image_urls && data.image_urls !== 'None') {
+        // destroys the current image carousel
         destroyCarousel();
     
         while (placeholder.firstChild) {
             placeholder.firstChild.remove();
         }
-    
+        
+        // populates the image carousel with images
         for (let imageUrl of data.image_urls) {
             let hoverData = data.treatment[imageUrl] ? data.treatment[imageUrl] : "";
             placeholder.appendChild(createImageContainer(imageUrl, hoverData));
         }
-    
+        
+        // takes care of multiple images
         if (data.image_urls.length > 1) {
             initCarousel();
     
@@ -426,6 +265,7 @@ function updateSpecimenData(data) {
     
             placeholder.appendChild(downloadButton);
     
+        // case of only one image
         } else if (data.image_urls.length === 1) {
             $(placeholder).find('.image-popup').on('click', function(e) {
                 e.preventDefault();
@@ -433,6 +273,7 @@ function updateSpecimenData(data) {
             });
         }
     
+    // if a specimen doesn't have any images, it will destroy the existing carousel and clear the placeholder
     } else {
         destroyCarousel();
         while (placeholder.firstChild) {
